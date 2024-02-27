2ed589f377c55dba66e7064826a3b7839bf4de2e
Carl Adams <carl51690@gmail.com>
2024-02-27 05:19:52 -0500

    myfile.md

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  const URL = "https://anapioficeandfire.com/api/books";
+  return fetch(URL)
+    .then(response => response.json())
+    .then(books => renderBooks(books))
+    .catch(error => console.error('Error fetching data: ', error));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+const main = document.querySelector('main');
+books.forEach(book => {
+const h2 = document.createElement('h2');
+h2.innerHTML = book.name;
+main.appendChild(h2);
+});
 }
 
 document.addEventListener('DOMContentLoaded', function() {
